cc7c7ca488e5c078aced91640404037de83e880e
mohibhaider1 <mohibhaider090@gmail.com>
2026-02-16 14:13:56 +0500

    Added Triangle class extending Shape

---
+++ app/src/main/java/com/example/commit/Triangle.java
@@ -0,0 +1,13 @@
+package com.example.commit;
+
+public class Triangle extends Shape {
+
+    private int base;
+    private int height;
+
+    public Triangle(int base, int height) {
+        this.base = base;
+        this.height = height;
+    }
+}
+
